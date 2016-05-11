994e3ce4d91d3b43394d8170f078ef0de90c40f5
RuneBarrett <runebarrett@gmail.com>
2016-05-11 14:51:47 +0200

    initial

---
+++ Assets/createBody.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createBody : MonoBehaviour {
+
+    Rigidbody rb;
+    RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rb == null) {
+            if (Physics.Raycast(transform.position, Vector3.down, out hit)){
+                print("Found an object - distance: " + hit.distance + " " + hit.transform);
+                gameObject.AddComponent<Rigidbody>();
+                rb = GetComponent<Rigidbody>();
+                rb.drag = 0.2f;
+                
+            }
+
+        }
+	}
+}
